513024985a2e3b250a3dfdafc61bba16f35f2163
Hee Jun Yi <junyi.hjy@gmail.com>
2015-05-26 14:21:58 +0800

    Fix typo for sys.platform

--- crawler/run.py
+++ crawler/run.py
@@ -52,7 +52,7 @@ def main():
             }
             pickle.dump(data, open(SESSION_P, "wb"))
 
-		if not sys.platform == 'win32':
+		if sys.platform is not 'win32':
 		    os.system("killall -9 phantomjs")
 
 	install_shutdown_handlers(handle_shutdown)
